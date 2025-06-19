543a93bf53d090852a42ae49082eaa3e3b9e31b6
Edudu111 <eduardosilva010503@gmail.com>
2025-06-18 22:14:32 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,6 @@
+function mostrarMensagem() {
+    const mensagemDiv = document.getElementById("mensagem");
+    mensagemDiv.innerHTML = "<p>Obrigado por visitar minha página! Tenha um ótimo dia!</p>";
+    mensagemDiv.style.fontWeight = "bold";
+    mensagemDiv.style.marginTop = "10px";
+}
